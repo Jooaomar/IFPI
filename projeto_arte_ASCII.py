6f94b0c1c119fa0aba3d3a86ea81cbaee8df59f1
JoaoMarcelo <jmrcl91@gmail.com>
2022-03-10 21:55:54 -0300

    Atividade: 01 Arte em ASCII

---
+++ projeto_arte_ASCII.py
@@ -0,0 +1,60 @@
+# Dizendo oi
+print("Oi, tudo bem?")
+
+# Desafio: No que você está pensando?
+print("Aprendendo ainda mais Python")
+
+# Arte ASCII
+print('''
+Uma imagem de um cachorro
+
+0____
+ ||||
+        
+''')
+
+# Desafio: Sobre você
+print('''
+Meu animal favorito é o gato:
+
+    _._     _,-'""`-._
+     (,-.`._,'(       |\`-/|
+         `-.-' \ )-`( , o o)
+     -bf-      `-    \`_`"'-
+
+'''
+)
+
+# Calculando textos
+print("Olá" * 5)
+print("ola, " + "tudo bem?" )
+
+# Desafio: palavras e números
+print("O"+"to"+"r"*2 + "i" + "no" + "la" + "rin" + "go" + "lo" + "gis" + "ta")
+print("ma" + "çã")
+
+# Padrões em ASCII
+# Dinossauro
+print('''
+
+            __
+           / _) 
+    .-^^^-/ /
+ __/       /
+<__.|_|-|_|
+
+''')
+
+print("E aqui vai um bolo!"+" " + "hu"+ "m"*6)
+print("") 
+print(" " + "^"*12)
+print("#"*15)
+print("="*15)
+print("#"*15)
+print("="*15)
+print("#"*15)
+
+
+
+
+
