c0c7a900515dfa31f9f72ea6b85ac80537934ed3
JoaoMarcelo <jmrcl91@gmail.com>
2022-03-22 23:55:37 -0300

    Escreva seu nome

---
+++ atividade-sem-03-T1-1/nome_sorbenome.py
@@ -0,0 +1,8 @@
+"""Escreva um programa que leia separadamente seu nome e o seu sobrenome e 
+mostre na tela o nome completo."""
+
+print("Escreva seu primeiro nome")
+nome = input()
+print("Escreva seu sobrenome")
+sobrenome = input()
+print("Seu nome completo é:", nome, sobrenome)
