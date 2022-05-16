08d4d00650f3beb14af5b8354bc1db39fa8c8f59
JoaoMarcelo <jmrcl91@gmail.com>
2022-05-16 10:57:53 -0300

    Atividade

--- PEC/projeto_05/06_adivinha.py
+++ PEC/projeto_05/06_adivinha.py
@@ -14,22 +14,28 @@ jogando    = True
 while jogando == True:
     
     print('\nSua pontuação é ', score)
-
+    
     v = input('Gostaria de somar mais um número? (s/n) ').lower() 
         
     if v == 's':
+        # Gerando número aleatório de 1 a 10
         n = randint(1,10)
+        # Acrescendo número aleatório ao score
         score += n
+        # Pontuação
         print('\nSua pontuação é ', score)
-
+    
+    # Caso não queira continuar encerrar jogo e pontuação final
     if v == 'n':
         print('Sua pontuação final é', score)
         jogando = False
-
+    
+    # Se acertar os 21 pontos usuário ganha jogo
     if score == 21:
         print("\n** Parabéns! Você fez 21 pontos **")
         jogando = False 
-
+    
+    # Se passar de 21 pontos, usuário perde jogo
     if score > 21:
         print("Que pena!!")
         jogando = False
